66e776fb722912606a4054acc1a2efc197b489ab
Adrian <aomm24@gmail.com>
2021-06-05 16:21:01 -0400

    Configuring React

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import { Navbar, NavbarBrand } from "reactstrap";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar dark color="primary">
+        <div className="container">
+          <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
+        </div>
+      </Navbar>
+      {/* <nav className="navbar navbar-dark navbar-expand-sm fixed-top">
+        <div className="container">
+          <a className="navbar-brand mr-auto" href="#">
+            Ristorante Con Fusion
+          </a>
+        </div>
+      </nav> */}
     </div>
   );
 }
