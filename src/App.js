adae9d1ce3797b7892c81162162f87d33e0fa7cc
Adrian <aomm24@gmail.com>
2021-06-06 11:38:14 -0400

    Components Part 1

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 import logo from "./logo.svg";
 import { Navbar, NavbarBrand } from "reactstrap";
 import "./App.css";
+import Menu from "./components/MenuComponent";
 
 function App() {
   return (
-    <div className="App">
+    <div>
       <Navbar dark color="primary">
         <div className="container">
           <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
         </div>
       </Navbar>
+      <Menu />
+      <h1>hola</h1>
       {/* <nav className="navbar navbar-dark navbar-expand-sm fixed-top">
         <div className="container">
           <a className="navbar-brand mr-auto" href="#">
