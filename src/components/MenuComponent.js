4b619804528556c0c2e920e6ddffdcbf8ae28bd7
Adrian <aomm24@gmail.com>
2021-07-01 17:36:07 -0400

    Components Part 2

--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -1,76 +1,68 @@
 import React, { Component } from "react";
 import { Media } from "reactstrap";
+import {
+  Card,
+  CardImg,
+  CardImgOverlay,
+  CardText,
+  CardBody,
+  CardTitle,
+} from "reactstrap";
 
 class Menu extends Component {
   constructor(props) {
     super(props);
+
     this.state = {
-      dishes: [
-        {
-          id: 0,
-          name: "Uthappizza",
-          image: "assets/images/uthappizza.png",
-          category: "mains",
-          label: "Hot",
-          price: "4.99",
-          description:
-            "A unique combination of Indian Uthappam (pancake) and Italian pizza, topped with Cerignola olives, ripe vine cherry tomatoes, Vidalia onion, Guntur chillies and Buffalo Paneer.",
-        },
-        {
-          id: 1,
-          name: "Zucchipakoda",
-          image: "assets/images/zucchipakoda.png",
-          category: "appetizer",
-          label: "",
-          price: "1.99",
-          description:
-            "Deep fried Zucchini coated with mildly spiced Chickpea flour batter accompanied with a sweet-tangy tamarind sauce",
-        },
-        {
-          id: 2,
-          name: "Vadonut",
-          image: "assets/images/vadonut.png",
-          category: "appetizer",
-          label: "New",
-          price: "1.99",
-          description:
-            "A quintessential ConFusion experience, is it a vada or is it a donut?",
-        },
-        {
-          id: 3,
-          name: "ElaiCheese Cake",
-          image: "assets/images/elaicheesecake.png",
-          category: "dessert",
-          label: "",
-          price: "2.99",
-          description:
-            "A delectable, semi-sweet New York Style Cheese Cake, with Graham cracker crust and spiced with Indian cardamoms",
-        },
-      ],
+      selectedDish: null,
     };
   }
-
-  render() {
-    const menu = this.state.dishes.map((dish) => {
+  // para cambiar el estado ( es llamada de ntro de la constante meny)
+  onDishSelect(dish) {
+    this.setState({ selectedDish: dish });
+  }
+  // para mostrar el que hacemos click es llamada en el render
+  renderDish(dish) {
+    if (dish != null)
       return (
-        <div key={dish.id} className="col-12 mt-5">
-          <Media tag="li">
-            <Media left middle>
-              <Media object src={dish.image} alt={dish.name} />
-            </Media>
-            <Media body className="ml-5">
-              <Media heading>{dish.name}</Media>
-              <p>{dish.description}</p>
-            </Media>
-          </Media>
+        <Card>
+          <CardImg top src={dish.image} alt={dish.name} />
+          <CardBody>
+            <CardTitle>{dish.name}</CardTitle>
+            <CardText>{dish.description}</CardText>
+          </CardBody>
+        </Card>
+      );
+    else return <div></div>;
+  }
+
+  // la funcion render se pone siempre para mostra el contenido
+  // el contenido es el return de esta funcion
+  // en este caso antes del return declaramos una constante menu que que devuelve las cards de todos los dishes
+
+  // porque la card no ocupan el 100% del ancho?
+  render() {
+    const menu = this.props.dishes.map((dish) => {
+      return (
+        <div className="col-12 col-md-5 m-3">
+          {/* con el click cambia el estado de selected dish de null al que cliqueamos */}
+          <Card key={dish.id} onClick={() => this.onDishSelect(dish)}>
+            <CardImg width="100%" src={dish.image} alt={dish.name} />
+            <CardImgOverlay>
+              <CardTitle>{dish.name}</CardTitle>
+            </CardImgOverlay>
+          </Card>
         </div>
       );
     });
 
     return (
       <div className="container">
+        <div className="row">{menu}</div>
         <div className="row">
-          <Media list>{menu}</Media>
+          <div className="col-12 col-md-5 m-5">
+            {this.renderDish(this.state.selectedDish)}
+          </div>
         </div>
       </div>
     );
